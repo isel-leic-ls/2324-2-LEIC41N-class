a0c5d51f7672c7aa1644550f87ae52e362839b35
GitHubFaustino <fstn75@gmail.com>
2025-02-25 00:58:00 +0000

    tests

--- src/main/kotlin/pt/isel/ls/AppDb.kt
+++ src/main/kotlin/pt/isel/ls/AppDb.kt
@@ -1,48 +1,123 @@
 package pt.isel.ls
 
 import org.postgresql.ds.PGSimpleDataSource
+import java.sql.Connection
+import java.sql.SQLException
 
-fun main(){
 
+public class AppDb {
     val dataSource = PGSimpleDataSource()
-    val jdbcDatabaseURL = System.getenv("JDBC_DATABASE_URL")
+    var jdbcDatabaseURL = System.getenv("JDBC_DATABASE_URL")
+
     //dataSource.setURL(jdbcDatabaseURL)
-    dataSource.setURL("jdbc:postgresql://localhost/postgres?user=postgres&password=postgres")
+    private lateinit var connection: Connection
 
-    // DELETE SNIPPET
-    dataSource.getConnection().use {
-        val stm = it.prepareStatement("delete from students where number =?;")
-        stm.setInt(1, 2201)
-
-        val rs = stm.execute();
+    // INIT
+    init {
+        // test only
+        jdbcDatabaseURL = "jdbc:postgresql://localhost/postgres?user=postgres&password=postgres"
+        dataSource.setURL(jdbcDatabaseURL)
     }
 
-    // INSERT SNIPPET
-    dataSource.getConnection().use {
-        val stm = it.prepareStatement("insert into students(course, number, name) values (?, ?, ?);")
-        stm.setInt(1, 1)
-        stm.setInt(2, 2200)
-        stm.setString(3, "Faustino")
-
-        val rs = stm.execute();
-    }
-    // UPDATE SNIPPET
-    dataSource.getConnection().use {
-        val stm = it.prepareStatement("update students set number=? where name =?")
-        stm.setInt(1, 2201)
-        stm.setString(2, "Faustino")
-
-        val rs = stm.execute();
-    }
-
-    // SELECT SNIPPET
-    dataSource.getConnection().use {
-        val stm = it.prepareStatement("select * from students")
-        val rs = stm.executeQuery()
-        while (rs.next()) {
-            println(rs.getString("name"))
+    // OPEN
+    fun openConnection(): Boolean {
+        println("Connection")
+        try {
+            connection = dataSource.connection
+        } catch (e: SQLException) {
+            println("Connection error: ${e.message}")
         }
+
+        return connection.isValid(0)
+    }
+
+    //CLOSE
+    fun closeConnection() {
+        if (connection.isValid(0) && !connection.isClosed) {
+            try {
+                connection.close()
+            } catch (e: SQLException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // DELETE
+    fun doDelete(record: Int): Boolean {
+        var error = false
+
+        openConnection()
+
+        try {
+            val stm = connection.prepareStatement("delete from students where number =?;")
+            stm.setInt(1, record)
+            val rs = stm.execute()
+        } catch (e: Exception) {
+            println("Connection error: ${e.message}")
+            error = true
+        } finally {
+            closeConnection()
+        }
+
+        return !error
+    }
+
+
+    fun doInsert(course:Int, number:Int, name:String): Boolean {
+        var error = false
+
+        openConnection()
+
+        try {
+            val stm = connection.prepareStatement("insert into students(course, number, name) values (?, ?, ?);")
+            stm.setInt(1, course)
+            stm.setInt(2, number)
+            stm.setString(3, name)
+            val rs = stm.execute()
+        } catch (e: Exception) {
+            println("Connection error: ${e.message}")
+            error = true
+        } finally {
+            closeConnection()
+        }
+
+        return !error
     }
 
 
 }
+
+
+    fun main() {
+
+
+        // INSERT SNIPPET
+        /*
+        dataSource.connection.use {
+            val stm = connection.prepareStatement("insert into students(course, number, name) values (?, ?, ?);")
+            stm.setInt(1, 1)
+            stm.setInt(2, 2200)
+            stm.setString(3, "Faustino")
+
+            val rs = stm.execute()
+        }
+        // UPDATE SNIPPET
+        dataSource.connection.use {
+            val stm = connection.prepareStatement("update students set number=? where name =?")
+            stm.setInt(1, 2201)
+            stm.setString(2, "Faustino")
+
+            val rs = stm.execute()
+        }
+
+        // SELECT SNIPPET
+        dataSource.connection.use {
+            val stm = connection.prepareStatement("select * from students")
+            val rs = stm.executeQuery()
+            while (rs.next()) {
+                println(rs.getString("name"))
+            }
+        }
+*/
+
+    }
--- src/test/kotlin/pt/isel/ls/ExampleTest.kt
+++ src/test/kotlin/pt/isel/ls/ExampleTest.kt
@@ -5,12 +5,28 @@ import java.io.FileInputStream
 import java.io.FileNotFoundException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import pt.isel.ls.AppDb
+import kotlin.test.assertTrue
 
 class ExampleTest {
 
 
     @Test
+    fun testDatabase() {
+         val db: AppDb = AppDb();
+
+        assertTrue { db.doDelete(2201) }
+        assertTrue { db.doDelete(2202) }
+        assertTrue { db.doInsert(1, 2201, "Faustino 2201") }
+        assertTrue { db.doInsert(1, 2202, "Faustino 2202") }
+
+    }
+
+
+    //@Test
     fun indexOfBinaryT() {
+
+
         assertEquals(indexOfBinary(intArrayOf(1, 2, 3, 4), 0, 1, 1),0);
         assertEquals(indexOfBinary(intArrayOf(1, 2, 3, 4), 0, 0, 4),-1);
         assertEquals(indexOfBinary(intArrayOf(1, 2, 3, 4), 1, 1, 4),-1);
